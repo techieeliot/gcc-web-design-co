c45f2d0138522566f8c491dcda7678250a15d6b7
Eliot Sanford <techieeliot@gmail.com>
2025-03-30 08:00:19 -0500

    style: update services layout

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -10,17 +10,19 @@ import { TableOfContents } from './TableOfContents.client'
 export default function PortfolioRoute() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="flex flex-col md:flex-row gap-8">
-        <div className="flex-1 max-w-4xl space-y-8">
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
+        {/* Main Content Column */}
+        <div className="lg:col-span-9 space-y-8">
           <CaseStudyFourthParty />
           <CaseStudyWeedWarriors />
           <CaseStudyGorillaFund />
           <CaseStudyDareToShare />
           <CaseStudyRoomInTheInn />
         </div>
-        <div className="w-64 lg:w-72 flex-shrink-0">
-          <TableOfContents className="top-20" />{' '}
-          {/* Add top spacing to avoid header overlap */}
+
+        {/* Table of Contents Column */}
+        <div className="lg:col-span-3">
+          <TableOfContents className="sticky top-20 bg-white p-6 rounded-lg shadow-md" />
         </div>
       </div>
     </div>
